a6afc144b27c8cb8a723515e21df4b0f004f8814
Jason Liu <jason.liu@oocl.com>
2018-11-26 15:20:13 +0800

    refactor the condidition age to a constant var

--- src/main/java/com/tws/refactoring/Police.java
+++ src/main/java/com/tws/refactoring/Police.java
@@ -1,8 +1,10 @@
 package com.tws.refactoring;
 
 public class Police {
+    private final int lowestAllowedAge = 18;
     public boolean checkDriver(Driver driver) {
-        if(driver.getAge() >= 18) return true;
+
+        if(driver.getAge() >= lowestAllowedAge) return true;
         else return false;
     }
 
